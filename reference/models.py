16c1be9f74a63d9a0c39fa6ad8cd2e8c7ccb4dfb
Sergey Maltsev <maltsevsergey3129@gmail.com>
2023-04-27 16:37:03 +0300

    flake8

--- reference/models.py
+++ reference/models.py
@@ -4,14 +4,33 @@ from django.db import models
 
 
 class Reference(models.Model):
-    code = models.CharField(max_length=100, unique=True)
-    name = models.CharField(max_length=300)
-    description = models.TextField()
+    """
+    Модель справочника.
+
+    Fields:
+        - code (CharField): уникальный код справочника
+        - name (CharField): наименование справочника
+        - description (TextField): описание справочника
+
+    Методы:
+        - __str__: возвращает наименование справочника
+        - get_current_version: возвращает текущую версию справочника
+    """
+    code = models.CharField(max_length=100, unique=True, verbose_name='Уникальный код справочника')
+    name = models.CharField(max_length=300, verbose_name='Наименование справочника')
+    description = models.TextField(verbose_name='Описание справочника')
 
     def __str__(self):
         return self.name
 
     def get_current_version(self):
+        """
+        Возвращает текущую версию справочника.
+
+        :returns:
+            - версия справочника (ReferenceVersion), если версия существует и её дата начала действия меньше или равна текущей дате
+            - None, если версия не существует или её дата начала действия больше текущей даты
+        """
         versions = ReferenceVersion.objects.filter(
             reference=self, start_date__lte=datetime.date.today()
         ).order_by('-start_date')
@@ -22,9 +41,23 @@ class Reference(models.Model):
 
 
 class ReferenceVersion(models.Model):
-    reference = models.ForeignKey(Reference, on_delete=models.CASCADE)
-    version = models.CharField(max_length=50)
-    start_date = models.DateField()
+    """
+    Модель версии справочника.
+
+    Fields:
+        - reference (ForeignKey): ссылка на справочник
+        - version (CharField): версия справочника
+        - start_date (DateField): дата начала действия версии
+
+    Meta:
+        - unique_together: уникальность записей по комбинации полей reference и start_date
+
+    Методы:
+        - __str__: возвращает версию справочника
+    """
+    reference = models.ForeignKey(Reference, on_delete=models.PROTECT, verbose_name='Внешний ключ на справочник')
+    version = models.CharField(max_length=50, verbose_name='Версия справочника')
+    start_date = models.DateField(verbose_name='Дата начала действия версии')
 
     class Meta:
         unique_together = ('reference', 'start_date')
@@ -34,9 +67,24 @@ class ReferenceVersion(models.Model):
 
 
 class ReferenceElement(models.Model):
-    version = models.ForeignKey(ReferenceVersion, on_delete=models.CASCADE)
-    code = models.CharField(max_length=100)
-    value = models.CharField(max_length=300)
+    """
+    Модель элемента справочника.
+
+    Fields:
+        - version (ForeignKey): ссылка на версию справочника
+        - code (CharField): код элемента справочника
+        - value (CharField): значение элемента справочника
+
+    Meta:
+        - unique_together: уникальность записей по комбинации полей version и code
+
+    Методы:
+        - __str__: возвращает код элемента справочника
+    """
+    version = models.ForeignKey(ReferenceVersion, on_delete=models.PROTECT,
+                                verbose_name='Внешний ключ на версию справочника')
+    code = models.CharField(max_length=100, verbose_name='Код элемента справочника')
+    value = models.CharField(max_length=300, verbose_name='Значение элемента справочника')
 
     class Meta:
         unique_together = ('version', 'code')
--- reference/serializers.py
+++ reference/serializers.py
@@ -3,24 +3,36 @@ from .models import Reference, ReferenceVersion, ReferenceElement
 
 
 class RefBookSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели справочника
+    """
     class Meta:
         model = Reference
         fields = ['id', 'code', 'name']
 
 
 class RefBookVersionSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели версии справочника
+    """
     class Meta:
         model = ReferenceVersion
         fields = ['version', 'start_date']
 
 
 class RefBookElementSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели элемента справочника
+    """
     class Meta:
         model = ReferenceElement
         fields = ['code', 'value']
 
 
 class ReferenceSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели справочника со всеми версиями
+    """
     versions = RefBookVersionSerializer(many=True, read_only=True)
 
     class Meta:
@@ -29,6 +41,9 @@ class ReferenceSerializer(serializers.ModelSerializer):
 
 
 class ReferenceVersionSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели версии справочника со всеми элементами
+    """
     elements = RefBookElementSerializer(many=True, read_only=True)
 
     class Meta:
